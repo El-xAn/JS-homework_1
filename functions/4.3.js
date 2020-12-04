1c7979d797299b431b72efe4233febc7b5f88a4c
El-xAn <hidefination123@gmail.com>
2020-12-04 14:01:25 +0400

    HW functions 3rd task in html

--- functions/4.3.js
+++ functions/4.3.js
@@ -2,10 +2,43 @@
 let units = ['zero','one','two','three','four','five','six','seven','eight','nine'];
 let ten = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
 let tenners = ['twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety'];
-let hundreds = ['one hundred','two','триста','четыреста','пятьсот','шестьсот','семьсот','восемьсот','девятьсот']; 
+    
+    let browinput = prompt('Enter number till 999 in english: ');
+    let input = browinput.toString();
+    let answer = 0;
 
-function numbers() {
-    switch(units) {
-        case 
+    for(let i = 0; i < 10; i++) {
+
+        if (input == units[i]) {
+            answer = i;
+        } else if (input == ten[i]) {
+            answer = i + 10;
+        } else if (input == tenners[i]) {
+            answer = (i + 2) * 10;
+        } 
+
+        for(let j = 0; j < 8; j++) {
+
+            if (input == tenners[j] + ' ' + units[i]) {
+                answer = (j+2) * 10 + i;
+            }
+
+            for (let l = 0; l < 10; l++) {
+           
+                if(input == units[l] + ' ' + 'hundred' + ' ' + tenners[j] + ' ' + units[i]) {
+                    answer = l * 100 +(j+2) * 10 + i;
+                } else if (input == units[l] + ' ' + 'hundred') {
+                    answer = l*100;
+                } else if(input == units[l] + ' ' + 'hundred' + ' ' + units[i]) {
+                    answer = l * 100 + i;
+                } else if (input == units[l] + ' ' + 'hundred' + ' ' + tenners[j]) {
+                    answer = l * 100 + (j + 2)*10
+                }                
+                
+            }
+        } 
     }
-}
+    
+    
+    
+alert(answer);
