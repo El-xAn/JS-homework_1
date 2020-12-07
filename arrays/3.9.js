0c6dfcac59e8039c065c537fb5af75ecbdd61a0d
El-xAn <hidefinatio123@gmail.com>
2020-12-07 09:19:39 +0400

    added sorting method insert

--- arrays/3.9.js
+++ arrays/3.9.js
@@ -7,20 +7,34 @@ for(let i = 0; i < 6; i++) {
     let num = getRandom(-10, 10)
     arr[i] = num;
 }
-console.log(arr);
+console.log(`массив: [${arr}]`);
 
-        for (let i = 0; i < arr.length; i++) {
+//сортировка массива пузырьком
 
-                let temp;
-
-                for (let j = i + 1; j < arr.length; j++) {
-                    if (arr[i] > arr[j]) {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
-            
+for (let i = 0; i < arr.length; i++) {
+    let temp
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
-
+    }        
+}
 console.log(`Oтсортированный массив (пузырьком (Bubble)): [${arr}]`)
+
+
+//сортировка массива вставками
+
+for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    
+    while (temp < arr[j]) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = temp;
+}
+
+console.log(`Oтсортированный массив (вставками (Insert)): [${arr}]`)
